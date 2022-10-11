17f693cc644bc7800dfc42dc49c7a9558ef06889
Maickel Pirchiner <maickelpc@gmail.com>
2022-10-10 21:37:21 -0300

    fix: Customer Seeder arrays

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -23,7 +23,7 @@ class CustomerSeeder extends Seeder
                 'birth_date' => '1985-10-31',
                 'gender' => 'MALE',
                 'address' => 'Rua das Couves, 12',
-                'city_id' => City::orderByRaw('RAND()')->first()->id,
+                'city_id' => City::orderByRaw('RAND()')->first()->id
             ],
 
             [
@@ -32,7 +32,7 @@ class CustomerSeeder extends Seeder
                 'birth_date' => '2001-05-19',
                 'gender' => 'FEMALE',
                 'address' => 'R. Ouro Verde, 1294 - AP 12',
-                'city_id' => City::orderByRaw('RAND()')->first()->id,
+                'city_id' => City::orderByRaw('RAND()')->first()->id
             ],
 
             [
@@ -41,7 +41,7 @@ class CustomerSeeder extends Seeder
                 'birth_date' => '2000-11-05',
                 'gender' => 'MALE',
                 'address' => 'Avenida Brasil, 900 - Casa dos Fundos',
-                'city_id' => City::orderByRaw('RAND()')->first()->id,
+                'city_id' => City::orderByRaw('RAND()')->first()->id
             ],
 
             [
@@ -50,7 +50,7 @@ class CustomerSeeder extends Seeder
                 'birth_date' => '2000-11-05',
                 'gender' => 'MALE',
                 'address' => 'Avenida Brasil, 900 - Casa dos Fundos',
-                'city_id' => City::orderByRaw('RAND()')->first()->id,
+                'city_id' => City::orderByRaw('RAND()')->first()->id
             ],
 
             [
@@ -59,7 +59,7 @@ class CustomerSeeder extends Seeder
                 'birth_date' => '1968-11-05',
                 'gender' => 'FEMALE',
                 'address' => 'Avenida Brasil, 900 - Casa dos Fundos',
-                'city_id' => City::orderByRaw('RAND()')->first()->id,
+                'city_id' => City::orderByRaw('RAND()')->first()->id
             ],
 
             [
@@ -68,7 +68,7 @@ class CustomerSeeder extends Seeder
                 'birth_date' => '1980-19-01',
                 'gender' => 'MALE',
                 'address' => 'Avenida Brasil, 900 - Casa dos Fundos',
-                'city_id' => City::orderByRaw('RAND()')->first()->id,
+                'city_id' => City::orderByRaw('RAND()')->first()->id
             ],
 
             [
@@ -77,7 +77,7 @@ class CustomerSeeder extends Seeder
                 'birth_date' => '1965-11-05',
                 'gender' => 'FEMALE',
                 'address' => 'Avenida Brasil, 900 - Casa dos Fundos',
-                'city_id' => City::orderByRaw('RAND()')->first()->id,
+                'city_id' => City::orderByRaw('RAND()')->first()->id
             ],
            
 
